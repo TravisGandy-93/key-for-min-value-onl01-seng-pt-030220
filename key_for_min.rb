93ead2c5ebdcb6b08df4ba82621705063e471a0e
Travis Gandy <travis_gandy@yahoo.com>
2020-03-16 15:28:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_box_label = nil
+  smallest_box_size = nil 
+  name_hash.each do |k, v|
+    if price < smallest_box_size 
+      smallest_box_size = v 
+      smallest_box_label = k
+  end 
+end 
 end
